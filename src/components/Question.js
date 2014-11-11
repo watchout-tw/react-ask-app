85a5bb41f311de3b6b8d4352ddd793484e07fbf7
Yuan Hsiang Cheng <lyforever62@hotmail.com>
2014-11-11 19:15:36 +0800

    chore(title): expend content when title clicking

--- src/components/Question.js
+++ src/components/Question.js
@@ -6,7 +6,14 @@ module.exports = React.createClass({
 
   getInitialState () {
     return {
+      hideContent: true
     };
+  },
+
+  _handleClick () {
+    this.setState({
+      hideContent: !this.state.hideContent
+    });
   },
 
   render () {
@@ -18,17 +25,7 @@ module.exports = React.createClass({
     // var paragraphs = data.content.map((p)=>{
     //   return <p className='q_text'>{p}</p>
     // });
-
-    return <div className='q_item md-whiteframe-z1 q_item_active'>
-      <div className='q_title l_inline ng-binding q_title_active'>
-        <div className='q_order l_inline'>{'1'}</div>
-        <div className='q_vote l_inline'>{'1,583'}</div>
-        {'你如何理解年輕人在台北市工作生活的巨大經濟壓力感?'}
-        <div className='q_function l_inline'>
-          <span><i className='fa fa-angle-double-down'></i></span>
-        </div>
-      </div>
-      <div className='q_content'>
+    var content = (state.hideContent)? '': <div className='q_content'>
         <p className='q_text'>{'你如何理解年輕人在台北市工作生活的巨大經濟壓力感?'}</p>
         <div className='q_postmeta'>{'提問人：someid / 提問時間：02-22 12:00'}</div>
         <div className='q_action'>
@@ -41,7 +38,19 @@ module.exports = React.createClass({
             {' 分享'}
           </a>
         </div>
+      </div>;
+    var itemClass = (state.hideContent)? 'q_item md-whiteframe-z1' : 'q_item md-whiteframe-z1 q_item_active';
+    var titleClass = (state.hideContent)? 'q_title l_inline' : 'q_title l_inline q_title_active';
+    return <div className={itemClass}>
+      <div className={titleClass} onClick={this._handleClick}>
+        <div className='q_order l_inline'>{'1'}</div>
+        <div className='q_vote l_inline'>{'1,583'}</div>
+        {'你如何理解年輕人在台北市工作生活的巨大經濟壓力感?'}
+        <div className='q_function l_inline'>
+          <span><i className='fa fa-angle-double-down'></i></span>
+        </div>
       </div>
+      {content}
     </div>;
   }
 });
