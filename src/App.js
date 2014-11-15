de5e5b42be3263ef1b89a1fc32737b1728fecba3
soidid <soidid@gmail.com>
2014-11-15 21:04:21 +0800

    add footer in App.js

--- src/App.js
+++ src/App.js
@@ -54,6 +54,23 @@ module.exports = React.createClass({
       <Navigation _toggleSiderBar={this._toggleSiderBar}
                   loggedIn={state.loggedIn} />
       <props.activeRouteHandler loggedIn={state.loggedIn} />
+      
+      <div id="footer">
+          <div className="footer_link">
+          <ul>
+              <li className="footer_text"><a href="#/terms">使用條款</a></li>
+              <li className="footer_text"><a href="#/how-to-ask">發問指南</a></li>
+              <li className="footer_text"><a href="#/contact">聯絡我們</a></li>
+          </ul>
+          </div>
+          <div className="footer_logo">
+          <ul>
+              <li className="footer_img"><a href="http://goo.gl/2vlnVR" target="_new"><img id="footer_apple" src="/assets/images/apple_logo.png" /></a></li>
+              <li className="footer_img"><a href="http://goo.gl/du71aO" target="_new"><img id="footer_watchout" src="/assets/images/watchout_logo_black.png" /></a></li>
+              <li className="footer_img"><a href="http://goo.gl/LB3KC9" target="_new"><img src="/assets/images/google_logo_color.png" /></a></li>
+          </ul>
+          </div>
+      </div>
     </div>;
 
     // return <div>
