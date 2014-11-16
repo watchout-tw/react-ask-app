b4b609c0e5d0374e84b545388a360bb6eaa1e905
Yuan Hsiang Cheng <lyforever62@hotmail.com>
2014-11-16 22:38:41 +0800

    fix(index.js): resolve correct html5shiv.js

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -4,7 +4,7 @@ require("../styles/style.css");
 require("../styles/font-awesome.min.css");
 require.context("../images", true, /\.(jpg|png)$/);
 require("es5-shim");
-require("html5shiv");
+require("../../node_modules/html5shiv/dist/html5shiv.js");
 
 var React = require("react/addons");
 var app_router = require("../../src/AppRouter");
