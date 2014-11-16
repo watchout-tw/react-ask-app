325d29b64e6a5e7a3c52654efff03a48bbc0067d
Yuan Hsiang Cheng <lyforever62@hotmail.com>
2014-11-16 22:15:14 +0800

    feat(IE8): try to support ie8

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,8 +1,10 @@
 "use strict";
-require("../styles/doc.css");
+require.context("../styles/doc.css");
 require("../styles/style.css");
 require("../styles/font-awesome.min.css");
 require.context("../images", true, /\.(jpg|png)$/);
+require("es5-shim");
+require(" html5shiv");
 
 var React = require("react/addons");
 var app_router = require("../../src/AppRouter");
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,7 @@
 var Path = require("path");
 var webpack = require("webpack");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
+var ExtractTextWebpackPlugin = require("extract-text-webpack-plugin");
 
 var JSX_LOADER = "jsx-loader?harmony";
 // var CSS_LOADER = "style-loader!css-loader";
@@ -17,7 +18,7 @@ var webpackConfig = module.exports = {
     loaders: [
       { test: require.resolve("react/addons"), loader: "expose-loader?React" },
       { test: /\.js(x?)$/, loader: JSX_LOADER },
-      { test: /\.css$/, loader: "style-loader!css-loader"},
+      { test: /\.css$/, loader: ExtractTextWebpackPlugin.extract('style-loader', 'css-loader') },//"style-loader!css-loader"},
       { test: /\.woff(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff&name=[path][name].[ext]?[hash]&context=client" },
       { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader?name=[path][name].[ext]?[hash]&context=client" },
       // { test: /\.less$/, loader: CSS_LOADER }
@@ -29,7 +30,8 @@ var webpackConfig = module.exports = {
     new HtmlWebpackPlugin({
       template: "./client/index.html",
       filename: "../app.html"
-    })
+    }),
+    new ExtractTextWebpackPlugin('[name].css')
   ]
 };
 
