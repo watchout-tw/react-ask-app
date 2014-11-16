468d8d4a848c499674fe62b9892a15521ab8c3bf
Yuan Hsiang Cheng <lyforever62@hotmail.com>
2014-11-16 22:31:18 +0800

    fix(index.js): fix wrong space

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -4,7 +4,7 @@ require("../styles/style.css");
 require("../styles/font-awesome.min.css");
 require.context("../images", true, /\.(jpg|png)$/);
 require("es5-shim");
-require(" html5shiv");
+require("html5shiv");
 
 var React = require("react/addons");
 var app_router = require("../../src/AppRouter");
