ee6e0c9d235192bc91bd49513c16d5193dad179e
soidid <soidid@gmail.com>
2014-11-20 18:10:42 +0800

    change index wording

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -36,11 +36,15 @@ module.exports = React.createClass({
             <div className="index_subsection">
                 <div className="index_maintitle">
                    <h1>李前市長給問嗎？</h1>
+                   <h3>92歲高齡 VS. 92年次首投族</h3>
                 </div>
                 <p>台北市長選舉在即，你/妳準備好要投給誰了嗎？沃草特別為大家邀請到「前」台北市長，也是前總統李登輝先生來接受網友們的提問挑戰！這是李登輝先生首次接受網路提問以及直播的邀約。兩個小時的問答，讓你/妳了解台北城發展的歷史脈絡以及李登輝先生經營城市的理念，也讓我們看看高齡九十一歲的李前總統如何應對新世代網友的提問挑戰！</p>
             </div>
             <div className="index_subsection">
-                <div className="index_title">活動說明</div>
+                <div className="index_title">
+                <h2>活動說明</h2>
+                <h3 className="index_maintitle_sub">只有三天，把握發問機會！</h3>
+                </div>
                 <p>即日起，針對五大主題：「城市發展、市政經營、國家前途思維、領導能力、世代正義」，在網站上提出你的問題，直播節目中，將依照連署數多寡，來讓李登輝先生現場回答鄉民的挑戰！</p>
                 <ul>
                   <li>提問時間：即日起～ 11/23 23:59</li>
