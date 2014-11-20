bedeecea5fae4d3dc6a50007272141c9e9fea8f2
Pomin Wu <pomin5@gmail.com>
2014-11-20 23:13:37 +0800

    move configurations to env vars

---
+++ server/config/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID,
+  FACEBOOK_APP_SECRET: process.env.FACEBOOK_APP_SECRET,
+  FACEBOOK_APP_CALLBACK: process.env.FACEBOOK_APP_CALLBACK,
+  database: process.env.MONGODB_URL,
+  sessionSecret: process.env.SESSION_SECRET,
+  tokenSecret: process.env.TOKEN_SECRET
+}
